1056ff33c83ee3bfb8545fc1dc822e7dee897898
JOTA <63990197+ujota@users.noreply.github.com>
2026-02-05 19:40:11 -0300

    Correções necessarias

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  define: {
+    'process.env': process.env
+  }
+});
